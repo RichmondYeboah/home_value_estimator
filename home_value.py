ec647f5ec836f8fed2f93e53bbc82cd1d3b350bd
Richmond <ryeboah@laisar.com>
2022-12-17 01:06:22 -0500

    Updated version: Added functions and test

--- home_value.py
+++ home_value.py
@@ -1,35 +1,107 @@
 class House:
-    def __init__(self, place, num_bedrooms, price):
+     # Create __init__ method 
+    # Set eight attributes place, bathrooms, num_bedrooms, location, price, house_type, condition
+    def __init__(self, name =str, place = str,  num_bedrooms = int, location = str, price = float, house_type = str, condition = str):
+
         self.place = place
         self.num_bedrooms = num_bedrooms
-        self.price = price
-        
-    def get_place(self):
-        return f'Your house in {self.place} cost: ${self.price}'
-    
-    def housing_tax(self,state):
-        return f'Your taxes for your house at {state} is 6% the actual price is: ${self.price*1.06}'
-    
-class dwelling(House):
-    def __init__(self, bathrooms = int, sqrt = 1_875, condition = 'good', location = 'city'):
-        self.bathrooms = bathrooms
-        self.sqrt_ft = sqrt
-        self.condition = condition
         self.location = location
+        self.price = price
+        self.house_type = house_type
+        self.condition = condition
+        self.name = name
+        
+    # In this function we will get input from the user
+    def house_info(self):
+        self.name = str(input("What is your name: "))
+        self.place = str (input(f'Hello {self.name}, what place in Maryland do you want to buy a house ? '))
+        self.location = str(input(' Are you looking for a house in suburbs or city ? : '))
+        self.house_type = str(input(' What type of house are you looking for (condo or single house) ? : '))
+        self.condition = str(input('What condition of the house are you looking for (good or excellent) ? : '))
+        self.num_bedrooms = int(input('How many bedrooms do you want in your house ? '))
+        
+        return self.location, self.house_type
+        
+        
+class dwelling(House):
+    #We will call the House class in the init method
+    def __init__(self, name, place,  num_bedrooms = int, location = str, price = float, house_type =str, condition = str):
+        super().__init__(name, place,  num_bedrooms = int, location = str, price = float, house_type =str, condition = str)
+  
+    #Calculate the house price based on the location
+    def location_estimator(self):
+        suburbs = 150_000 
+        city = 250_000 
+        
+        if self.location == "suburbs":
+            self.price = suburbs
+        elif self.location == "city":
+            self.price = city
     
-    def location_estimator(location):
-        suburbs = 440_000
-        city = 650_000
-        if location == "suburbs":
-            return f'your house will cost less than ${suburbs}'
-        else:
-            return f'Your house will cost more than ${city}'
-def main(): 
-    print(housing_boston.get_place())
-    print(housing_boston.housing_tax("Rockville"))
-    print(housing_location_estimator.location_estimator("city"))
+            
+        return self.price
     
+    #Calculate the house price based on the home type
+    def house_type_estimator (self):
+        condo = 325_000
+        single_house = 500_000
+        if self.house_type == "condo":
+            self.price += condo
+        elif self.house_type == "single house":
+            self.price += single_house
+        return self.price
+    
+    #Calcualte the house price based on the condition of the house
+    def condition_estimator (self):
+        good = 50_000
+        excellent = 75_000
+        
+        if self.condition == "good":
+            self.price += good
+            
+        elif self.condition == "excellent":
+            self.price += excellent
+            
+        return self.price
+    
+    #Calculate the house price based on the number of bedrooms 
+    def bedroom_estimator (self):
+        two_bed = 5000
+        three_bed = 10000
+        
+        if self.num_bedrooms <= 2:
+            self.price += two_bed
+            
+        elif self.num_bedrooms > 2 :
+            self.price += three_bed
+            
+        return self.price
+              
+    #Calculate the house tax based on the total price of the house    
+    def housing_tax(self):
+        
+        total = float(self.price*1.06)
+        
+        return f'The tax rate for the {self.house_type} in {self.place}, Maryland is 6%. The total price with taxes is: ${total}'
+    
+
+ # main() function print out all the methods       
+def main():  
+   
+    dwelling_housing.house_info()
+    dwelling_housing.location_estimator()
+    dwelling_housing.house_type_estimator()
+    dwelling_housing.condition_estimator()
+    dwelling_housing.bedroom_estimator()
+    print("\n")
+    print(dwelling_housing.housing_tax())
+    print("\n")
+  
+    
+        
+#Create instance of House class and Dwelling Class
+#Call the main function
 if __name__ =='__main__':
-    housing_boston = House("Rockville, Maryland", 6, 550_000 )
-    housing_location_estimator = dwelling
+    dwelling_housing = dwelling(name = str, place = str, num_bedrooms = int, location = str, price = float,  condition = str)
+    housing_location_estimator = House(name = str, place = str, num_bedrooms = int, location = str, price = float)
     main()
--- test_house_PYTEST.py
+++ test_house_PYTEST.py
@@ -1,7 +1,33 @@
 import home_value
-    
-def test_house():
+    #test_get_place
+def test_housing_input():
     '''Testing that the House class has the correct parameters'''
-    home = home_value.House("Rockville, Maryland", 6, 550_000)
-    assert home.place == "Rockville, Maryland", "The name may not have been set correctly"
- 
+    #Passed the argument in the house class
+    home = home_value.House(name = 'Ari', place = 'Rockville', num_bedrooms = 6, location = 'suburbs',  condition = 'good', house_type='condo')
+    #Assert the place to see if the place name is set correctly
+    assert home.place == "Rockville", "The place may not have been set correctly"
+    assert home.name == "Ari", "The name may not have been set correctly"
+    assert home.house_type == "condo", "The house type may not have been set correctly"
+    assert home.condition == "good", "The condition may not have been set correctly"
+    assert home.num_bedrooms == 6, "The number of bedrooms may not have been set correctly"
+    assert home.location == "suburbs", "The location may not have been set correctly"
+
+
+def test_location_estimator():
+    '''Testing that the dwelling class has the correct parameters'''
+    #Passed the arguments in the dwelling class
+    estimator = home_value.House( name= 'Ari', place='Rockville',  location = 'suburbs', price = 150_000)
+    #Called the location_estimator()
+    # estimator.location_estimator()
+    #Assert the location to see if the location is set correctly
+    assert estimator.price == 150_000, "The price type may not have been set correctly"
+    
+     
+
+    
+def test_condition_estimator():
+    #Passed the argument in the dwellingclass
+    condition = home_value.House( name = 'Ari', place = 'Rockville',  price = 250_000, num_bedrooms = 6, house_type='condo', location = 'suburbs',  condition = 'Good')
+    #Called the condition_estimator method
+    #Assert condition to see if the price is set correctly
+    assert condition.price == 250_000, "The price may not have been set correctly"
